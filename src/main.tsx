1fbef59aa08f20884f4967f82cb8ebfba1247173
SECURITASOLIVIER <olivier.marchegay@securitas.fr>
2026-09-22 19:52:48 +0200

    V2 preview: use hash routing for static hosting

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './auth/AuthProvider'
 import App from './App'
@@ -10,9 +10,9 @@ const queryClient=new QueryClient({defaultOptions:{queries:{staleTime:15000,retr
 ReactDOM.createRoot(document.getElementById('root')!).render(
  <React.StrictMode>
   <QueryClientProvider client={queryClient}>
-   <BrowserRouter basename={import.meta.env.BASE_URL}>
+   <HashRouter>
     <AuthProvider><App/></AuthProvider>
-   </BrowserRouter>
+   </HashRouter>
   </QueryClientProvider>
  </React.StrictMode>
 )
