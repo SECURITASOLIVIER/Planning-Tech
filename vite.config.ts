b626d712aa03f3c477eb80ed0c116add58eae0aa
SECURITASOLIVIER <olivier.marchegay@securitas.fr>
2026-09-22 19:52:51 +0200

    V2 preview: use relative asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({command})=>({
+export default defineConfig({
   plugins:[react()],
-  base: command==='build' ? '/Planning-Tech/' : '/',
+  base:'./',
   build:{outDir:'dist',sourcemap:true}
-}))
+})
